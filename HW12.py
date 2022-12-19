a63133d2c54f0d2f8ebeeafd3420e349912ee790
Marina Alieva <120053456+mgitalieva@users.noreply.github.com>
2022-12-19 19:24:15 +0300

    Create HW12.py

---
+++ HW12.py
@@ -0,0 +1,9 @@
+money=int(input("Сумма вклада:"))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit=[]
+deposit.append(round(money*per_cent['ТКБ']/100))
+deposit.append(round(money*per_cent['СКБ']/100))
+deposit.append(round(money*per_cent['ВТБ']/100))
+deposit.append(round(money*per_cent['СБЕР']/100))
+
+print("Максимальная сумма, которую вы можете заработать —", max(deposit))
